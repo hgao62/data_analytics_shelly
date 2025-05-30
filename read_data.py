f12008369ea9a92a700761352fdf3a5c25fa2bf8
Shelly Huang <shellyh06026@gmail.com>
2025-05-30 00:42:16 -0400

    task 1

--- read_data.py
+++ read_data.py
@@ -1,2 +1,10 @@
-# test comment
-    
+import pandas as pd
+stock_data = pd.read_csv("/Users/shellyh/data_analytics_shelly/stock_data.csv")
+print(stock_data)
+start_date= 
+end_date=
+get_stock_data= stock_data[stock_data[date]>start_date,stock_data[date]<end_date]
+sector_data = pd.read_csv("/Users/shellyh/data_analytics_shelly/sector_info.csv")
+print(sector_data)
+get_sector_data= sector_data[["Ticker", "Sector", "Industry"]]
+print(get_sector_data)
